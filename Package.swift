0a1d157fa65a236da38acf87833d497e56d59880
Jay Buddhdev <jay.b@simformsolutions.com>
2024-03-01 19:20:48 +0530

    UNT-T22917: Added Alamofire in Package dependency

--- Package.swift
+++ Package.swift
@@ -13,6 +13,7 @@ let package = Package(
             targets: ["NetworkEngine"]),
     ],
     dependencies: [
+         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.8.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
